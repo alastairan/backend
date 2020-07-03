4cb7c12a83681f1d234f83eb051275ff98c35649
unknown <you@example.com>
2020-07-03 15:18:39 +1000

    backend commit

---
+++ backend/Controllers/Models/Quiz.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Controllers.Models
+{
+    public class Quiz
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string OwnerId { get; set; }
+    }
+}
